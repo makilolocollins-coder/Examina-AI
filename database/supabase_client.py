8ccf49a3b402908c1082a1923a7304fc12447d46
Collins Oghenegharovwe Makilolo <makilolocollins@gmail.com>
2026-09-01 13:07:22 +0100

    Update supabase_client.py

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -2,27 +2,20 @@ import streamlit as st
 from supabase import create_client
 
 
+@st.cache_resource
 def get_supabase_client():
 
-    try:
-        database_url = st.secrets["DATABASE_URL"]
-        database_key = st.secrets["DATABASE_KEY"]
-
-    except KeyError as error:
-        missing_key = error.args[0]
-
-        raise RuntimeError(
-            f"Streamlit Secret '{missing_key}' is missing."
-        )
+    database_url = st.secrets.get("DATABASE_URL")
+    database_key = st.secrets.get("DATABASE_KEY")
 
     if not database_url:
         raise RuntimeError(
-            "DATABASE_URL is empty."
+            "DATABASE_URL is missing from Streamlit Secrets."
         )
 
     if not database_key:
         raise RuntimeError(
-            "DATABASE_KEY is empty."
+            "DATABASE_KEY is missing from Streamlit Secrets."
         )
 
     return create_client(
