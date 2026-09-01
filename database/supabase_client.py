12a9c4cfe2316bd04563ce3d73b780079e2ba840
Collins Oghenegharovwe Makilolo <makilolocollins@gmail.com>
2026-09-01 13:51:34 +0100

    Update supabase_client.py

--- database/supabase_client.py
+++ database/supabase_client.py
@@ -1,10 +1,22 @@
+# ============================================================
+# EXAMINA AI
+# SUPABASE CLIENT
+# ============================================================
+
 import streamlit as st
+
 from supabase import create_client
 
 
+# ============================================================
+# GET SUPABASE CLIENT
+# ============================================================
+
+@st.cache_resource
 def get_supabase_client():
 
     try:
+
         database_url = st.secrets["DATABASE_URL"]
         database_key = st.secrets["DATABASE_KEY"]
 
@@ -17,11 +29,13 @@ def get_supabase_client():
         )
 
     if not database_url:
+
         raise RuntimeError(
             "DATABASE_URL is empty."
         )
 
     if not database_key:
+
         raise RuntimeError(
             "DATABASE_KEY is empty."
         )
